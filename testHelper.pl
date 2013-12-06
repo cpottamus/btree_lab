547e0ceb55201672058628ce29ea169707ef57b3
Carson Potter <cp3192@gmail.com>
2013-12-06 17:56:52 -0600

    specified tester

--- testHelper.pl
+++ testHelper.pl
@@ -10,19 +10,19 @@ system "makedisk testdisk 1024 128 1 1024 1 100 10 .28";
 system "btree_init testdisk 1024 4 4";
 
 
-my @commands = ("btree_insert testdisk 1024 1111 7777",
-"btree_insert testdisk 1024 aaaa 0000",
-"btree_insert testdisk 1024 bbbb 1111",
-"btree_insert testdisk 1024 cccc 2222",
-"btree_insert testdisk 1024 dddd 3333",
-"btree_insert testdisk 1024 eeee 4444",
-"btree_insert testdisk 1024 ffff 5555",
-"btree_insert testdisk 1024 gggg 6666",
-"btree_insert testdisk 1024 2222 8888",
-"btree_insert testdisk 1024 3333 9999",
-"btree_insert testdisk 1024 4444 aaaa");
+my @commands = ("btree_insert testdisk 1024 1 val1",
+"btree_insert testdisk 1024 2 val2",
+"btree_insert testdisk 1024 3 val3",
+"btree_insert testdisk 1024 4 val4",
+"btree_insert testdisk 1024 5 val5",
+"btree_insert testdisk 1024 6 val6",
+"btree_insert testdisk 1024 7 val7",
+"btree_insert testdisk 1024 8 val8",
+"btree_insert testdisk 1024 9 val9",
+"btree_insert testdisk 1024 10 vala",
+"btree_insert testdisk 1024 11 valb");
 
-fisher_yates_shuffle(\@commands);
+#fisher_yates_shuffle(\@commands);
 
 foreach my $command(@commands){
 	print "::::::::::::::::~ Insert Command ~::::::::::::::::\n";
