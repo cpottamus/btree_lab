975d4da38af543f95adcb7c4bfba93b780097818
Carson Potter <cp3192@gmail.com>
2013-12-06 14:37:37 -0600

    variable declaration efficiency in testHelper.pl

--- testHelper.pl
+++ testHelper.pl
@@ -10,19 +10,17 @@ system "makedisk testdisk 1024 128 1 1024 1 100 10 .28";
 system "btree_init testdisk 1024 4 4";
 
 
-my $cmd1 = "btree_insert testdisk 1024 1111 7777";
-my $cmd2 = "btree_insert testdisk 1024 aaaa 0000";
-my $cmd3 =  "btree_insert testdisk 1024 bbbb 1111";
-my $cmd4 =  "btree_insert testdisk 1024 cccc 2222";
-my $cmd5 =  "btree_insert testdisk 1024 dddd 3333";
-my $cmd6 =  "btree_insert testdisk 1024 eeee 4444";
-my $cmd7 =  "btree_insert testdisk 1024 ffff 5555";
-my $cmd8 =  "btree_insert testdisk 1024 gggg 6666";
-my $cmd9 =  "btree_insert testdisk 1024 2222 8888";
-my $cmd10 =  "btree_insert testdisk 1024 3333 9999";
-my $cmd11 =  "btree_insert testdisk 1024 4444 aaaa";
-
-my @commands = ($cmd1, $cmd2, $cmd3, $cmd4, $cmd5, $cmd6, $cmd7, $cmd8, $cmd9, $cmd10, $cmd11);
+my @commands = ("btree_insert testdisk 1024 1111 7777",
+"btree_insert testdisk 1024 aaaa 0000",
+"btree_insert testdisk 1024 bbbb 1111",
+"btree_insert testdisk 1024 cccc 2222",
+"btree_insert testdisk 1024 dddd 3333",
+"btree_insert testdisk 1024 eeee 4444",
+"btree_insert testdisk 1024 ffff 5555",
+"btree_insert testdisk 1024 gggg 6666",
+"btree_insert testdisk 1024 2222 8888",
+"btree_insert testdisk 1024 3333 9999",
+"btree_insert testdisk 1024 4444 aaaa");
 
 fisher_yates_shuffle(\@commands);
 
